a736f4d02e8788df685fff2eff067f965c35bcb5
synergy2411 <synergy2411@outlook.com>
2019-12-03 10:18:52 +0530

    two way binding

--- users-app/src/app/app.module.ts
+++ users-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
@@ -10,7 +11,8 @@ import { UserComponent } from './user/user.component';
     UserComponent
   ],
   imports: [              // to import & register all modules
-    BrowserModule
+    BrowserModule,
+    FormsModule 
   ],
   providers: [],          // services
   bootstrap: [AppComponent]         //
--- users-app/src/app/user/user.component.ts
+++ users-app/src/app/user/user.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector : 'app-user',
+  selector: 'app-user',
   // template :`
   //   <h1>User Component Loaded Successfully.</h1>
   // `
-  templateUrl : './user.component.html'
+  templateUrl: './user.component.html'
 })
-export class UserComponent{
+export class UserComponent {
   user = {
-    firstName : "Bill",
-    lastName : "Gates",
-    dob : new Date("Dec 2, 1965"),
-    company : "Microsoft",
-    isWorking : true,
-    income : 50000
+    firstName: "Bill",
+    lastName: "Gates",
+    dob: new Date("Dec 2, 1965"),
+    company: "Microsoft",
+    isWorking: true,
+    income: 50000,
+    image: "assets/images/bill.jpg",
+    vote: 120
+  }
+
+  onMoreInfo(user: any) {
+    alert(`Mr. ${user.firstName} is working with ${user.company}!!`);
   }
 }
