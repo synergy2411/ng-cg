2bdadcc6efce865b442758ecf7bafbf10056c8ad
synergy2411 <synergy2411@outlook.com>
2019-12-03 14:15:45 +0530

    custom directives added

--- users-app/src/app/directives/highlight.directive.ts
+++ users-app/src/app/directives/highlight.directive.ts
@@ -1,11 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector : "[appHighlight]"
 })
 export class HighlightDirective{
+  @HostBinding('style.background-color') bgColor : any;
+
+  @Input('myColor') myColor : string;
+
+  @HostListener('mouseenter')
+  onmouseenter(){
+    this.bgColor = this.myColor;
+  }
+
+  @HostListener('mouseleave')
+  onmouseleave(){
+    this.bgColor = "transparent";
+  }
+
   constructor(private elRef : ElementRef){
-    // console.log(this.elRef.nativeElement);
-    this.elRef.nativeElement.style.backgroundColor = "lightgrey";
+    // this.elRef.nativeElement.style.backgroundColor = "lightgrey";
   }
 }
