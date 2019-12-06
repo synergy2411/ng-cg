e40213e5ac2e985648d40c0b04dae8deddeace7b
synergy2411 <synergy2411@outlook.com>
2019-12-06 13:02:17 +0530

    Angular tips and tricks

---
+++ playground/js-basics.js
@@ -0,0 +1,11 @@
+let user = {
+    name : "Foo"
+}
+
+// Changing the value in reference
+user.name = "Bar";          
+
+// Changing the reference
+user = {
+    name : "Bam"
+}
--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { DataService } from './data.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit{
   user = { name: "Foo" };
   person : {name : string};
 
-  constructor(private dataService : DataService){}
+  todos = [{label : "to pot the plant"}, {label : "to renew the insurance"}];
 
+  onNewArray(){
+    this.todos = [
+      {label : "to pot the plant"},
+      {label : "to renew the insurance"},
+      {label : "New Item"}
+    ]
+  }
+
+  trackFunc(index, label){
+    return index;
+  }
+
+  onAddNew(){
+    this.todos.push({label : "New Item"});
+  }
+
+  htmlSnippets = "HTML5 Snippets <script>javascript:alert('Cross Site Scripting')</script>";
+
+  dangerUrl = "javascript:alert('Hello World!')";
+  safeUrl : any;
+
+  constructor(private dataService : DataService,
+              private sanitize : DomSanitizer,
+              private cdRef : ChangeDetectorRef){
+                this.safeUrl = this.sanitize.bypassSecurityTrustUrl(this.dangerUrl);
+              }
+
+
+  onDetech(){
+    this.cdRef.detach();
+  }
+  onReattach(){
+    this.cdRef.reattach();
+  }
   ngOnInit(){
     this.person = this.dataService.getName();
   }
